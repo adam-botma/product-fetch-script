58093444251eb8daff496eca8050f49f8c04f102
adz <adz@adzs-Mac-mini-31.lan>
2024-07-10 09:49:11 -0700

    clean up

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const {displayProductInfo} = require('./utils/index')
 const args = process.argv.slice(2)
 const nameArgIndex = args.indexOf("--name")
 
-
 //check for --name flag and search criteria then display results
 if (nameArgIndex !== -1 && args[nameArgIndex + 1]) {
   const productName = args[nameArgIndex + 1]
